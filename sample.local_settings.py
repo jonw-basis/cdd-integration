4620743a11086121ba74d8d86ddad15757c36e75
Jon Weiss <jweiss@eqrx.com>
2023-11-24 07:48:56 -0500

    adjust code to handle issues encountered in initial run

---
+++ sample.local_settings.py
@@ -0,0 +1,17 @@
+
+#CDD
+CDD_VAULT_ID = ""
+
+CDD_KEY = ""
+
+CDD_PROJECT = {'name': "",
+                    'data_url': "",
+                    'webhook_url': ""}
+
+
+
+#EGNYTE
+EGNYTE_DOMAIN = ".egnyte.com"
+EGNYTE_ACCESS_TOKEN = ""
+EGNYTE_BASE_PATH = "/Shared/External"
+
